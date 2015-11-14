ebb79b7b3ab5e590e9664b72dd20c050c7baf582
Alice Wu <alcwu7@yahoo.com>
2015-11-14 09:50:04 -0500

    upload 11/14

--- FtcRobotController/src/main/java/com/qualcomm/ftcrobotcontroller/opmodes/WuOpmode.java
+++ FtcRobotController/src/main/java/com/qualcomm/ftcrobotcontroller/opmodes/WuOpmode.java
@@ -13,7 +13,6 @@ public class WuOpmode extends OpMode{
     DcMotor backright, backleft, frontleft, frontright;
     Servo wheelTest;
     int wheelPos;
-
     double future;
 
     public WuOpmode(){
@@ -33,10 +32,12 @@ public class WuOpmode extends OpMode{
     }
     public void loop()
     {
+
         // throttle: left_stick_y ranges from -1 to 1, where -1 is full up, and
         // 1 is full down
         // direction: left_stick_x ranges from -1 to 1, where -1 is full left
         // and 1 is full right
+
         float direction = gamepad1.left_stick_x;
         // clip the right/left values so that the values never exceed +/- 1
         direction = Range.clip(direction, 0, 1);
@@ -47,56 +48,20 @@ public class WuOpmode extends OpMode{
         //left =  (float)scaleInput(left);
 
         // update the position of the arm.
-        if (gamepad1.left_bumper) {
-            // if the A button is pushed on gamepad1, increment the position of
-            // the arm servo.
-
-            if(future < time) {
-                future = time + .1;
-
-
-                wheelPos--;
-
-                if(wheelPos <= -1)
-                    wheelPos = 0;
-                wheelPos = Math.abs(wheelPos);
-
-            }
-            /*if (wheelPos > 0) {
-                wheelPos -= 0.1;
-            }*/
-            telemetry.addData("Text", "LEFT BUMPER");
-
-        }
-
-        if (gamepad1.right_bumper) {
-            // if the Y button is pushed on gamepad1, decrease the position of
-            // the arm servo.
-
-            if(future< time) {
-                future = time + .1;
-                wheelPos++;
-            }
-            /*if (wheelPos < 1) {
-                wheelPos += 0.1;
-            }*/
-
-            if(wheelPos >= 21)//lenght of the array, change if you cahnge the array
-                wheelPos = 20;
-            telemetry.addData("Text", "RIGHT BUMPER");
-
-        }
-
+        /*
         if(gamepad1.left_trigger != 0){
-
+            telemetry.addData("leftTrig", "leftTrig:  " + gamepad1.left_trigger);//String.format("%.2f", wheelPos));
         }
 
         if(gamepad1.right_trigger != 0){
+            telemetry.addData("rightTrig", "rightTrig:  " + gamepad1.right_trigger);//String.format("%.2f", wheelPos));
 
-
-        }
-        //incretments from .2 straight to .5
+        }*/
+        //increments from .2 straight to .5
        // wheelPos = Range.clip(wheelPos, .2, .5);
+
+        turnXAxis(gamepad1.left_bumper, gamepad1.right_bumper);
+        //moveYAxis(gamepad1.left_trigger, gamepad1.right_trigger);
 
         // write position values to the wrist and claw servo
         wheelTest.setPosition(scaleWheelPos(wheelPos));
@@ -108,12 +73,15 @@ public class WuOpmode extends OpMode{
 		 * will return a null value. The legacy NXT-compatible motor controllers
 		 * are currently write only.
 		 */
-
+        /*
         telemetry.addData("Text", "*** Robot Data***");
-        telemetry.addData("wheel", "wheel:  " + wheelPos);//String.format("%.2f", wheelPos));
+        telemetry.addData("wheel", "wheel:  " + String.format("%.2f", wheelPos));
         //telemetry.addData("time", "time:  " + String.format("%.2f", time));
         //telemetry.addData("future", "future:  " + String.format("%.2f", future));
         telemetry.addData("future", "RAF POWER:  " + String.format("%.2f", scaleWheelPos(wheelPos)));
+        */
+        update_gamepad_telemetry();
+
     }
 
     public void stop() {
@@ -160,5 +128,51 @@ public class WuOpmode extends OpMode{
         int val = servoValue % values.length;
         return values[val];
     }
+
+    private void turnXAxis (boolean left, boolean right) {
+
+        if (left) {
+            if (future < time) {
+                future = time + .1;
+
+
+                wheelPos--;
+
+                if (wheelPos <= -1)
+                    wheelPos = 0;
+                wheelPos = Math.abs(wheelPos);
+            }
+        }
+
+        if (right) {
+            if(future < time) {
+                future = time + .1;
+                wheelPos++;
+            }
+            /*if (wheelPos < 1) {
+                wheelPos += 0.1;
+            }*/
+
+            if(wheelPos >= 21) {//lenght of the array, change if you cahnge the array
+                wheelPos = 20;
+            }
+
+        }
+    }
+    public void update_gamepad_telemetry ()
+    {
+        //
+        // Send telemetry data concerning gamepads to the driver station.
+        //
+        telemetry.addData ("01", "GP1 Left: " + -gamepad1.left_stick_x);
+        telemetry.addData ("02", "GP1 Right: " + -gamepad1.right_stick_y);
+        telemetry.addData ("03", "GP1 LT: " + gamepad1.left_trigger);
+        telemetry.addData ("04", "GP1 RT: " + gamepad1.right_trigger);
+        telemetry.addData ("05", "GP1 B: " + gamepad1.b);
+        telemetry.addData ("06", "GP1 A: " + gamepad1.a);
+        telemetry.addData ("07", "GP1 X: " + gamepad1.x);
+
+
+    } // update_gamepad_telemetry
 }
 
