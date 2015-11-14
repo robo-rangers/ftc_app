4ac9c21ff45def761fd1a3d090c1226a2338116f
Alice Wu <alcwu7@yahoo.com>
2015-11-14 12:18:20 -0500

    upload 11/14

--- FtcRobotController/src/main/java/com/qualcomm/ftcrobotcontroller/opmodes/WuOpmode.java
+++ FtcRobotController/src/main/java/com/qualcomm/ftcrobotcontroller/opmodes/WuOpmode.java
@@ -46,12 +46,13 @@ public class WuOpmode extends OpMode{
         // the robot more precisely at slower speeds.
         direction = (float)scaleInput(direction);
 
-        turnXAxis(gamepad1.left_bumper, gamepad1.right_bumper);
+        wheelPos = turnXAxis(gamepad1.left_bumper, gamepad1.right_bumper, wheelPos);
         //moveYAxis(gamepad1.left_trigger, gamepad1.right_trigger);
 
         // write position values to the wrist and claw servo
         wheelTest.setPosition(scaleWheelPos(wheelPos));
         updateGamepadTelemetry();
+
 
     }
 
@@ -100,45 +101,55 @@ public class WuOpmode extends OpMode{
         return values[val];
     }
 
-    private void turnXAxis (boolean left, boolean right) {
+    private int turnXAxis (boolean left, boolean right, int pos) {
 
         if (left) {
             if (future < time) {
                 future = time + .1;
 
 
-                wheelPos--;
+                pos--;
 
-                if (wheelPos <= -1)
-                    wheelPos = 0;
-                wheelPos = Math.abs(wheelPos);
+                //if (wheelPos <= -1)
+                //    wheelPos = 0;
+                pos = Math.abs(pos);
             }
         }
 
         if (right) {
             if(future < time) {
                 future = time + .1;
-                wheelPos++;
+                pos++;
             }
 
-            if(wheelPos >= 21) {//lenght of the array, change if you cahnge the array
+            /*if(wheelPos >= 21) {//lenght of the array, change if you cahnge the array
                 wheelPos = 20;
-            }
+            }*/
         }
+        return pos;
     }
 
     private void updateGamepadTelemetry ()
     {
 
         // Send telemetry data concerning gamepads to the driver station.
-        telemetry.addData ("01", "GP1 Left: " + -gamepad1.left_stick_x);
-        telemetry.addData ("02", "GP1 Right: " + -gamepad1.right_stick_y);
-        telemetry.addData ("03", "GP1 LT: " + gamepad1.left_trigger);
-        telemetry.addData ("04", "GP1 RT: " + gamepad1.right_trigger);
+        telemetry.addData ("01", "GP1 LeftHor: " + gamepad1.left_stick_x);
+        telemetry.addData ("02", "GP1 LeftVert: " + -gamepad1.left_stick_y);
+        telemetry.addData ("03", "GP1 RightHor: " + gamepad1.right_stick_x);
+        telemetry.addData ("04", "GP1 RightVert: " + -gamepad1.right_stick_y);
         telemetry.addData ("05", "GP1 B: " + gamepad1.b);
         telemetry.addData ("06", "GP1 A: " + gamepad1.a);
         telemetry.addData ("07", "GP1 X: " + gamepad1.x);
-
+        telemetry.addData ("08", "GP1 A: " + gamepad1.y);
+        telemetry.addData ("09", "GP1 bDPADL: " + gamepad1.dpad_left);
+        telemetry.addData ("10", "GP1 bDPADR: " + gamepad1.dpad_right);
+        telemetry.addData ("11", "GP1 bDPADUP: " + gamepad1.dpad_up);
+        telemetry.addData ("12", "GP1 bDPADDOWN: " + gamepad1.dpad_down);
+        telemetry.addData ("13", "GP1 LEFTB: " + gamepad1.left_stick_button);
+        telemetry.addData ("14", "GP1 RIGHTB: " + gamepad1.right_stick_button);
+        telemetry.addData ("15", "GP1 RTRIG: " + gamepad1.left_trigger);
+        telemetry.addData ("16", "GP1 LTRIG: " + gamepad1.right_trigger);
+        telemetry.addData ("WHEEL", "WHEELPOS:" + wheelPos);
 
     }
 }
